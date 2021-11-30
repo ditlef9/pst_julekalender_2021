df127e0162771d385fc8f68c5280e55b8d9733cf
Sindre <sindredit@gmail.com>
2021-11-30 21:44:06 +0100

    Meny

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,43 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+/*
+ Filename: Main.kt
+ Author: Mr X
+ Date: 30.11.2021 21:43
+ Version: 1
+ License: http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+fun main(args: Array<String>) {
+    // Menu
+    var endProgram = -1;
+    var continueProgram = -1;
+    while (endProgram <= 0) {
+        println("--- \uD83C\uDF85 PST Xmas Calendar 2021 \uD83C\uDF85 ---")
+        println("Select your luke between 1-24. Write anything else to end the program.")
+        print("Selection: ")
+        val menu = readLine()
+
+        // Go trough menu possibilities
+        if (menu.equals("1")) {
+            println("\n--- Luke 1 ---")
+
+            // Continue
+            println("Press [Enter] to continue")
+            val continueProgram = readLine()
+        } else if (menu.equals("2")) {
+            println("\n--- Luke 2 ---")
+
+            // Continue
+            println("Press [Enter] to continue")
+            val continueProgram = readLine()
+        } else if (menu.equals("3")) {
+            println("\n--- Luke 3 ---")
+
+            // Continue
+            println("Press [Enter] to continue")
+            val continueProgram = readLine()
+        } else {
+            endProgram = 1;
+        }
+    }
 }
